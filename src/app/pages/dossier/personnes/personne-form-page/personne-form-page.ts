2c3febd7d532e04c1065e612b66b4b2437f88f6c
TevaLautier <teva.lautier@gmail.com>
2026-02-18 09:44:41 +0100

    feat: add sexe

--- src/app/pages/dossier/personnes/personne-form-page/personne-form-page.ts
+++ src/app/pages/dossier/personnes/personne-form-page/personne-form-page.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { DsfrFormInputComponent } from "@edugouvfr/ngx-dsfr";
+import { DsfrFormInputComponent, DsfrFormRadioComponent,DsfrRadio } from "@edugouvfr/ngx-dsfr";
 import { AutoErrorMessageDirective } from '../../../../ui/forms/auto-error-message-directive';
 
 @Component({
   selector: 'app-personne-form-page',
-  imports: [DsfrFormInputComponent, ReactiveFormsModule],  
+  imports: [DsfrFormInputComponent, ReactiveFormsModule,DsfrFormRadioComponent],  
   templateUrl: './personne-form-page.html',
   styleUrl: './personne-form-page.css',
 })
 export class PersonneFormPage {
   form=new FormGroup({
-    nom:new FormControl('',Validators.required)
+    nom:new FormControl('',Validators.required),
+    sexe:new FormControl('',Validators.required),
   });
+  sexeOptions= signal<DsfrRadio[]>([
+    {value:"M",label:"Masculin"}, {value:"F",label:"Feminin"}
+  ]);
 }
